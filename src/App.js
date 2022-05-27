c6913aa8f6806e4becaa8de596aee9e6ded7415e
Stanimir <stanimirkosev00@gmail.com>
2022-05-27 14:39:05 +0300

    set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import background from "./images/background.jpg";
 
 function App() {
+  // func for valid click
+  // identify each char and save to database
+  // time
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img alt="background-img" src={background} />
     </div>
   );
 }
